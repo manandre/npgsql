cb4dce6aa639739a8504108eec4e8af88615808c
Emmanuel ANDRE <2341261+manandre@users.noreply.github.com>
2024-11-10 01:00:05 +0100

    Instrument NpgsqlBinaryImporter

--- src/Npgsql/NpgsqlActivitySource.cs
+++ src/Npgsql/NpgsqlActivitySource.cs
@@ -1,4 +1,4 @@
-﻿using Npgsql.Internal;
+using Npgsql.Internal;
 using System;
 using System.Data;
 using System.Diagnostics;
@@ -142,6 +142,28 @@ static class NpgsqlActivitySource
         activity.AddEvent(activityEvent);
         var statusDescription = ex is PostgresException pgEx ? pgEx.SqlState : ex.Message;
         activity.SetStatus(ActivityStatusCode.Error, statusDescription);
+        activity.Dispose();
+    }
+
+    internal static Activity? ImportStart(string copyFromCommand, NpgsqlConnectionStringBuilder settings)
+    {
+        var dbName = settings.Database ?? "UNKNOWN";
+
+        var activity = Source.StartActivity(dbName, ActivityKind.Client);
+
+        if (activity is not { IsAllDataRequested: true })
+            return activity;
+
+        activity.SetTag("db.statement", copyFromCommand);
+        activity.SetTag("db.operation", "COPY FROM");
+
+        return activity;
+    }
+
+    internal static void ImportStop(Activity activity, ulong rows)
+    {
+        activity.SetTag("otel.status_code", "OK");
+        activity.SetTag("db.rows", rows);
         activity.Dispose();
     }
 
